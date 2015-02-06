dc5f73762b22b22e401f4eee104fba5b27f9f2e0
roellinger <roellinger.robin@gmail.com>
2015-02-06 16:28:59 +0100

    tweet

---
+++ Projet_tweet/accueil.php
@@ -0,0 +1,37 @@
+<?php
+include("model/user.php");
+
+$accueil = new user();
+if(isset($_POST['deco'])){
+
+$accueil->deconnexion($_POST['deco']);
+}
+
+$accueil1 = $accueil->activate();
+
+if(isset($_SESSION['nom'])){
+
+foreach($accueil1 as $v){ 
+
+if($v['activate'] == 1) {
+
+?>
+
+<form action="accueil.php" method="post">
+
+<input type="submit" name="deco" />
+
+
+</form>
+
+<?php  
+
+} else {
+
+header("location: start.php");
+
+} } }else {
+
+header("location: index.php");
+
+} ?>
